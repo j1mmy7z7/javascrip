3457701bd53834abdddf058fc329242dcf8675f0
j1mmy7z7 <{ID}+{username}@users.noreply.github.com>
2023-08-29 12:48:45 +0300

    remove prompt, added new buttons, changed application logic

--- rock.js
+++ rock.js
@@ -4,60 +4,72 @@ function getComputerChoice () {
     return choice;
 }
 
-let win = 0
-let lose = 0
-let tie = 0
+const body = document.querySelector('body');
 
+let player = 0;
+let computer = 0;
+let tie = 0;
+
+
+function reset () {
+    player = 0;
+    computer = 0;
+    tie = 0;
+}
 
 function playround (playerSelection, computerSelection) {
-      if (playerSelection === computerSelection) {
-        result = "its a draw";
-        tie += 1
-      } else if (playerSelection === 'rock' ) {
-        if (computerSelection === 'paper' ) {
-            result = "you lose, paper covers rock ";
-            lose += 1
-        } else if (computerSelection === 'scissor') {
-            result = "you win, rock breaks scissor";
-            win += 1
-        }
-      } else if (playerSelection === 'paper') {
-        if (computerSelection === 'rock' ) {
-            result = "you win, paper covers rock";
-            win += 1 
-        } else if (computerSelection === 'scissor') {
-            result = "You lose, scissor cuts paper";
-            lose += 1
-        }
-      } else if (playerSelection === 'scissor') {
-        if (computerSelection === 'paper') {
-            result = "you win, scissor cuts paper";
-            win += 1
-        }else if (computerSelection === 'rock') {
-            result = "you lose, rock breaks scissor";
-            lose += 1
-        }
+    if (playerSelection === computerSelection) {
+      result = "its a draw";
+      tie += 1
+    } else if (playerSelection === 'rock' ) {
+      if (computerSelection === 'paper' ) {
+          result = "you lose, paper covers rock ";
+          computer += 1;
+      } else if (computerSelection === 'scissor') {
+          result = "you win, rock breaks scissor";
+          player += 1;
+      }
+    } else if (playerSelection === 'paper') {
+      if (computerSelection === 'rock' ) {
+          result = "you win, paper covers rock";
+          player += 1; 
+      } else if (computerSelection === 'scissor') {
+          result = "You lose, scissor cuts paper";
+          computer += 1;
+      }
+    } else if (playerSelection === 'scissor') {
+      if (computerSelection === 'paper') {
+          result = "you win, scissor cuts paper";
+          player += 1;
+      }else if (computerSelection === 'rock') {
+          result = "you lose, rock breaks scissor";
+          computer += 1;
+      }
+  }
+  const div = document.createElement('div');
+  if (player === 5 || computer === 5) {
+      if (player > computer) {
+        div.textContent = `You won , the results are your score ${player} to computer score ${computer}`;
+        div.style.padding = `10px`;
+        body.appendChild(div);
+        reset();
+      } else if ( computer > player) {
+        div.textContent = `you lost , the results  are your score ${player} to computer score ${computer}`;
+        div.style.color = `red`;
+        div.style.padding = `12px`;
+        body.appendChild(div);
+        reset();
+      }
     }
-
-    return result
+    
 }
 
-function play () {
-    for (let i=1; i<=5; i++) {
-        let pick = prompt("please pick between rock, paper and scissor");
-        playround(pick.toLowerCase(), getComputerChoice());
-    }
-    if (win > lose) {
-        return `You have won by ${win}`;
-    }else if (lose > win) {     
-        return `You lost by ${lose} much`;
-    }else if (lose === 0 && tie === 0) {
-        return "FLawless Victory";
-    }else {
-        return "Tie"
-    }
+function logValue(e) {
+    console.log(playround(this.classList.value, getComputerChoice()));
 }
 
+const buttons = document.querySelectorAll('button');
+buttons.forEach(button => button.addEventListener('click', logValue));
 
-console.log(play())
-console.log(`Win:${win}, Lose:${lose}, Draw:${tie}`)
+
+
